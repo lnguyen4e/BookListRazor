917dc9895b358be88671ad1efb01fc9f5adc87a9
Lam Nguyen <l0nguy10@louisville.edu>
2021-01-04 21:45:40 -0500

    57

---
+++ BulkBookDataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BulkBookDataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ BulkyBookUtility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BulkyBookUtility
+{
+    public class Class1
+    {
+    }
+}
